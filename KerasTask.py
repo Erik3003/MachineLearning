f2ec5f21d63ef9b364c264438020da87b0ab12f4
Erik3003 <erik.t.3003@googlemail.com>
2020-11-30 12:50:24 +0100

    Added KerasTask

---
+++ KerasTask.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data(path='mnist.npz')
+
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+model = Sequential()
+model.add(layers.Input(shape=(28,28)))
+model.add(layers.Dense(12))
+model.add(layers.Dense(10))
+
+opt = keras.optimizers.Adam(learning_rate=0.5)
+
+model.compile(optimizer=opt)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=100)
+model.evaluate()
